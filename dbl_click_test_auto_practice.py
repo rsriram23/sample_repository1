5ca65f2cd71cfc7674eb1ad525b730666eed7843
Bharathisbhuvi <bharathisbhuvi@gmail.com>
2026-06-02 16:42:15 +0530

    changed in file dbl

--- dbl_click_test_auto_practice.py
+++ dbl_click_test_auto_practice.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
 dr=webdriver.Chrome()
-dr.get("https://testautomationpractice.blogspot.com/")
+dr.get('https://testautomationpractice.blogspot.com/')
 # print('field 1->',dr.find_element("id","field1").get_attribute('value'))
 # dr.find_element("id","field1").click()
 sleep(5)
 act_obj=ActionChains(dr)
-act_obj.double_click(dr.find_element("xpath","//button[text()='Copy Text']")).perform()
+act_obj.double_click(dr.find_element('xpath',"//button[text()='Copy Text']")).perform()
 sleep(2)
-print('field 2->',dr.find_element("id","field2").get_attribute('value'))
+print('field 2->',dr.find_element('id',"field2").get_attribute('value'))
 sleep(10)
-print('Enabled webhook for github auto triggering jenkins')
+print("Enabled webhook for github auto triggering jenkins")
